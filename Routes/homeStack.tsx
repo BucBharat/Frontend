6b84a5777a12850555cbb0c54b1b37626732eecc
JBharat2001 <bharat.buchupalle1234@gmail.com>
2021-03-09 10:19:45 +0530

    added notification along with buying page on market side

--- Routes/homeStack.tsx
+++ Routes/homeStack.tsx
@@ -1,86 +1,86 @@
-<<<<<<< HEAD
-import { createStackNavigator} from 'react-navigation-stack';
-import {createAppContainer} from 'react-navigation';
-import MyItems from '../screens/MyItems';
-import ItemDetails from '../screens/ItemDetails';
-import AddItem from '../screens/AddItem';
-import StartScreen from '../screens/StartScreen'
-import RegisterScreen from '../screens/RegisterScreen'
-// import { RegisterScreen } from '../screens';
-import LoginScreen from '../screens/LoginScreen'
-import CardDetails from '../screens/CardDetails'
-import Product from '../components/Product'
-import { Dashboard, ForgotPasswordScreen } from '../screens';
-import BuyerOrSeller from '../screens/BuyerOrSeller'
-import Market from '../screens/Market';
-import BuyProduct from '../screens/BuyProduct'
+// <<<<<<< HEAD
+// import { createStackNavigator} from 'react-navigation-stack';
+// import {createAppContainer} from 'react-navigation';
+// import MyItems from '../screens/MyItems';
+// import ItemDetails from '../screens/ItemDetails';
+// import AddItem from '../screens/AddItem';
+// import StartScreen from '../screens/StartScreen'
+// import RegisterScreen from '../screens/RegisterScreen'
+// // import { RegisterScreen } from '../screens';
+// import LoginScreen from '../screens/LoginScreen'
+// import CardDetails from '../screens/CardDetails'
+// import Product from '../components/Product'
+// import { Dashboard, ForgotPasswordScreen } from '../screens';
+// import BuyerOrSeller from '../screens/BuyerOrSeller'
+// import Market from '../screens/Market';
+// import BuyProduct from '../screens/BuyProduct'
 
 
-const screens = {
-    StartScreen : {
-        screen : StartScreen,
-        navigationOptions: {
-            headerShown: false
-        }
-    },
-    RegisterScreen : {
-        screen : RegisterScreen
-    },
-    LoginScreen : {
-        screen : LoginScreen
-    },
-    ForgotPasswordScreen : {
-        screen : ForgotPasswordScreen
-    },
-    Dashboard : {
-        screen : Dashboard
-    },
-    MyItems: {
-        screen: MyItems,
-        navigationOptions: {
-            headerShown: false
-        }
-    },
-    AddItem: {
-        screen: AddItem
-    },
-    ItemDetails: {
-        screen: ItemDetails
-    },
-    CardDetails : {
-        screen: CardDetails,
-        navigationOptions: {
-            headerShown: false
-        }
-    },
-    BuyerOrSeller : {
-        screen: BuyerOrSeller,
-        navigationOptions: {
-            headerShown: false
-        }
-    },
-    Market : {
-        screen : Market,
-        navigationOptions: {
-            headerShown: false
-        }
-    },
-    BuyProduct : {
-        screen: BuyProduct,
-        navigationOptions: {
-            headerShown: false
-        }
-    },
+// const screens = {
+//     StartScreen : {
+//         screen : StartScreen,
+//         navigationOptions: {
+//             headerShown: false
+//         }
+//     },
+//     RegisterScreen : {
+//         screen : RegisterScreen
+//     },
+//     LoginScreen : {
+//         screen : LoginScreen
+//     },
+//     ForgotPasswordScreen : {
+//         screen : ForgotPasswordScreen
+//     },
+//     Dashboard : {
+//         screen : Dashboard
+//     },
+//     MyItems: {
+//         screen: MyItems,
+//         navigationOptions: {
+//             headerShown: false
+//         }
+//     },
+//     AddItem: {
+//         screen: AddItem
+//     },
+//     ItemDetails: {
+//         screen: ItemDetails
+//     },
+//     CardDetails : {
+//         screen: CardDetails,
+//         navigationOptions: {
+//             headerShown: false
+//         }
+//     },
+//     BuyerOrSeller : {
+//         screen: BuyerOrSeller,
+//         navigationOptions: {
+//             headerShown: false
+//         }
+//     },
+//     Market : {
+//         screen : Market,
+//         navigationOptions: {
+//             headerShown: false
+//         }
+//     },
+//     BuyProduct : {
+//         screen: BuyProduct,
+//         navigationOptions: {
+//             headerShown: false
+//         }
+//     },
     
-}
+// }
 
-const HomeStack = createStackNavigator(screens, {
-    defaultNavigationOptions: {
+// const HomeStack = createStackNavigator(screens, {
+//     defaultNavigationOptions: {
 
-    }
-});
+//     }
+// });
 
-=======
+// =======
 import { createStackNavigator} from 'react-navigation-stack';
 import {createAppContainer} from 'react-navigation';
 import MyItems from '../screens/MyItems';
@@ -181,5 +181,5 @@ const HomeStack = createStackNavigator(screens, {
     }
 });
 
->>>>>>> added cod button in the payment
+// >>>>>>> added cod button in the payment
 export default createAppContainer(HomeStack);
